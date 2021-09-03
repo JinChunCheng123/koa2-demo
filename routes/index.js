6c047e2fb4e8d113767cd1fc4492e796d6982b18
jinchuncheng <jinchuncheng@shuidi-inc.com>
2021-09-03 12:01:34 +0800

    A  add lib

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')()
-const index = require('../controllers/index_controller')
-router.get('/', index.index)
-router.get('/about',index.about)
-router.get('/world', index.world)
-router.post('/postdata', index.postdata)
-
-module.exports = router
