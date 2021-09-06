d5a27efff3552c2659b8a913313f60fc66a4c24f
jinchuncheng <jinchuncheng@shuidi-inc.com>
2021-09-06 15:42:19 +0800

    U update code

--- app.js
+++ app.js
@@ -11,25 +11,27 @@ const host = 'localhost'
 //添加中间件
 app.use(async (ctx,next) => {
   console.log('\x1B[31m%s\x1B[0m','1 start')
-  const startTime=new Date()
   await next()
-  let ms=new Date()-startTime
-  console.log('waste time:',ms)
   console.log('\x1B[31m%s\x1B[0m','1 end')
 
 });
 app.use(async (ctx,next) => {
   console.log('\x1B[31m%s\x1B[0m','2 start')
-  for(let i=0;i<100000000;i++){}
   await next()
   console.log('\x1B[31m%s\x1B[0m','2 end')
   
 });
 app.use(async (ctx,next) => {
   console.log('\x1B[31m%s\x1B[0m','3 start')
+  ctx.body='Hello World'
   console.log('\x1B[31m%s\x1B[0m','3 end')
 
+});
+app.on('error', err => {
+  console.error('server error', err)
 });
 app.listen(port, host, () => {
   console.log(`API server listening on ${host}:${port}`);
 });
+
+
