bd2723e4faff891e82b5f765e4c723315c041600
jinchuncheng <jinchuncheng@shuidi-inc.com>
2021-09-07 14:54:47 +0800

    U   update code

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const Koa = require("koa")
-const routes = require("./routes")
-const parameter = require("koa-parameter")
-const BodyParser = require("koa-bodyparser")
-const DB = require("./db")
+const Koa = require('koa')
+const path = require('path')
+const bodyParser = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+// 引入 koa-static
+const staticFiles = require('koa-static')
+
 const app = new Koa()
+const router = require('./router')
 
-app.use(BodyParser())
-app.use(parameter(app))
+// 指定 public目录为静态资源目录，用来存放 js css images 等
+app.use(staticFiles(path.resolve(__dirname, "./public")))
 
-routes(app)
-app.listen(7777)
+app.use(nunjucks({
+  ext: 'html',
+  path: path.join(__dirname, 'views'),
+  nunjucksConfig: {
+    trimBlocks: true
+  }
+}));
+
+app.use(bodyParser())
+router(app)
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
+})
