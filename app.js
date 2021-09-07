a4492e51a99e2945a5fc707abbedbf44842e2fb4
jinchuncheng <jinchuncheng@shuidi-inc.com>
2021-09-07 15:10:08 +0800

    U   update code

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
-/**
- * @Description: 
- * @Author: jinchuncheng
- * @Date: 2021-09-03
- */
+const Koa = require('koa')
+const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
 
- const Koa = require('koa')
- const routes= require('./routes')
- // 注意 require('koa-router') 返回的是函数:
- const router = require('koa-router')()
- const app = new Koa()
- 
-//   // 添加路由
-//   router.get('/', async (ctx, next) => {
-//      ctx.body = `<h1>index page</h1>`
-//  })
- 
-//  router.get('/home', async (ctx, next) => {
-//      ctx.body = '<h1>HOME page</h1>'
-//  })
- 
-//  router.get('/404', async (ctx, next) => {
-//      ctx.body = '<h1>404 Not Found</h1>'
-//  })
- 
-//   // 调用路由中间件
-//   app.use(router.routes())
-routes(app)
- app.listen(3000)
+app.use(bodyParser())
+
+router.get('/', async(ctx, next) => {
+  ctx.response.body = `<h1>index page</h1>`
+})
+
+router.get('/home', async(ctx, next) => {
+  console.log(ctx.request.query)
+  console.log(ctx.request.querystring)
+  ctx.response.body = '<h1>HOME page</h1>'
+})
+
+router.get('/home/:id/:name', async(ctx, next)=>{
+  console.log(ctx.params)
+  ctx.response.body = '<h1>HOME page /:id/:name</h1>'
+})
+
+router.get('/404', async(ctx, next) => {
+  ctx.response.body = '<h1>404 Not Found</h1>'
+})
+
+router.get('/user', async(ctx, next)=>{
+  ctx.response.body = 
+  `
+    <form action="/user/login" method="post">
+      <input name="name" type="text" placeholder="请输入用户名：jcc"/> 
+      <br/>
+      <input name="password" type="text" placeholder="请输入密码：123456"/>
+      <br/> 
+      <button>login</button>
+    </form>
+  `
+})
+
+// 增加响应表单请求的路由
+router.post('/user/login',async(ctx, next)=>{
+    console.log(ctx.request.body)
+  let {name, password} = ctx.request.body
+  if( name === 'jcc' && password === '123456' ){
+    ctx.response.body = `Hello， ${name}！` 
+  }else{
+    ctx.response.body = '账号信息错误'
+  }
+})
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
+})
--- routes/home.js
+++
@@ -1,9 +0,0 @@
-/**
- * @Description: 
- * @Author: jinchuncheng
- * @Date: 2021-09-04
- */
-const router = require("koa-router")()
-const { index } = require("../controllers/home")
-router.get("/", index)
-module.exports = router
