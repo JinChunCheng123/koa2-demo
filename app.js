733a38ac70d2a4c8c3da0d6f22cb4e0d5e745b47
jinchuncheng <jinchuncheng@shuidi-inc.com>
2021-09-08 19:58:07 +0800

    U  upload static code

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const Koa = require('koa')
+const static = require('koa-static')
 const path = require('path')
-const bodyParser = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
-// 引入 koa-static
-const staticFiles = require('koa-static')
+const app= new Koa()
+//静态资源目录相对于入口文件的相对地址
+const staticPath='./static'
+app.use(static(path.resolve(__dirname,staticPath)))
 
-const app = new Koa()
-const router = require('./router')
-
-// 指定 public目录为静态资源目录，用来存放 js css images 等
-app.use(staticFiles(path.resolve(__dirname, "./public")))
-
-app.use(nunjucks({
-  ext: 'html',
-  path: path.join(__dirname, 'views'),
-  nunjucksConfig: {
-    trimBlocks: true
-  }
-}));
-
-app.use(bodyParser())
-router(app)
-app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')
+app.listen(3000,()=>{
+  console.log('static is starting on port 3000')
 })
---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * @Description: 
+ * @Author: jinchuncheng
+ * @Date: 2021-09-08
+ */
+ (function( ){
+    alert('hello koa2 static server')
+    console.log('hello koa2 static server')
+  })()
